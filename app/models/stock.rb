5a47ed303e4bf20af1ad9665e944e06bb34bc8c5
Javier Jimenez <javierrcc522@gmail.com>
2017-12-18 19:55:30 -0800

    got response for the api call and parse the jSON file and added a tick param in string interpolation format

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock
-  def api_call(company)
-    response =  RestClient.get('https://api.robinhood.com/quotes/' + company + '/')
-  JSON.parse(response)
+  def api_call(ticker)
+    response =  RestClient.get("https://api.robinhood.com/quotes/#{ticker}/")
+    JSON.parse(response)
   end
 end

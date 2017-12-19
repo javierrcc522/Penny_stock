69c88d4712cb252c7d230c7b42e7fd99abb3545d
Javier Jimenez <javierrcc522@gmail.com>
2017-12-19 10:40:16 -0800

    change the names of my variable to make them more flexible so i could call one value in multiple and use that in multiple API calls

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,11 @@
 class Stock
-  def api_call(ticker)
+  def get_price(ticker)
     response =  RestClient.get("https://api.robinhood.com/quotes/#{ticker}/")
+    JSON.parse(response)
+  end
+
+  def get_detail(ticker)
+    response =  RestClient.get("https://api.robinhood.com/fundamentals/?symbols=#{ticker}")
     JSON.parse(response)
   end
 end

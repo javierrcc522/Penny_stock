12dd8064d6c18726a70a68cd9ed1bab228441de9
Javier Jimenez <javierrcc522@gmail.com>
2017-12-21 16:11:25 -0800

    cant leave empty

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,10 @@
 class StocksController < ApplicationController
   def index
     stock = Stock.new()
-    if params[:ticker]
+    if params[:ticker] and !params[:ticker].empty?
       @price = stock.get_price(params[:ticker])
       @detail = stock.get_detail(params[:ticker])
       @input = stock.get_name(params[:ticker])
     end
-
   end
 end
